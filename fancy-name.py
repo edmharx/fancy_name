7504f9faaaf471ddd44135f16b6c683d84c377df
edmharx <edmharxx@gmail.com>
2024-03-23 20:10:13 +0800

    Finalizing the style

--- fancy-name.py
+++ fancy-name.py
@@ -9,7 +9,24 @@ name = input("Kindly type your name: ")
 job = input("What is your dream job?: ")
 
 #creating input for age, sex, location
-age = int(input("How old are you?: "))
+
+age = input("How old are you?: ")
 sex = input("State you sex: ")
 loc = input("Where do you live?: ")
+
+
+#creating the art
+
+name_art = text2art(name, font="bubblehead")
+job_art = decor("wave2") + text2art(job, font="vaporwave") + decor("wave2", reverse=True)
+age_art = decor("line1") + text2art(age, font="fancy132") + decor("line1", reverse=True)
+sex_art = decor("line1") + text2art(sex, font="fancy132") + decor("line1", reverse=True)
+loc_art = decor("line1") + text2art(loc, font="fancy132") + decor("line1", reverse=True)
+
+#printing
+print(name_art)
+print(job_art)
+print(age_art)
+print(sex_art)
+print(loc_art)
 

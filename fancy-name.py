27d5d92dc093a124b5927fbc4deef3a6e3c40f9e
edmharx <edmharxx@gmail.com>
2024-03-23 19:46:23 +0800

    creating inputs

--- fancy-name.py
+++ fancy-name.py
@@ -1,7 +1,15 @@
 #this is a pseudocode
 #importing art module
 from art import *
+
 #creating input for the name
+name = input("Kindly type your name: ")
+
 #creating input for the dream job
+job = input("What is your dream job?: ")
+
 #creating input for age, sex, location
+age = int(input("How old are you?: "))
+sex = input("State you sex: ")
+loc = input("Where do you live?: ")
 
